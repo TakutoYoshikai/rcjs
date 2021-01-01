da965c9eddc8e6cac992cf52e76552e53880de33
lucky <takuto.yoshikai@gmail.com>
2021-01-01 22:11:27 +0900

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const RemoconModuleType = {
+  BUTTON: "button",
+}
+class RemoconModule {
+  constructor(id, type) {
+    this.id = id;
+    this.type = type;
+  }
+}
+
+class Button extends RemoconModule {
+  constructor(id, title, callback) {
+    super(id, RemoconModuleType.BUTTON);
+    this.title = title;
+    this.callback = callback;
+  }
+}
+function Remocon() {
+  const express = require("express");
+  const app = express();
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.set("view engine", "ejs");
+  let modules = {};
+  app.get("/", function(req, res) {
+    res.render("./index.ejs", {
+      modules: modules
+    });
+  })
+  app.post("/event", function(req, res) {
+    modules[req.body.id].callback();
+    res.status(200).send("");
+  });
+  this.addButton = function(id, title, callback) {
+    modules[id] = new Button(id, title, callback);
+  }
+  this.app = app;
+}
+
+module.exports = Remocon;
