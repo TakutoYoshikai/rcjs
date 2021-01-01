8c18abb5eb3bd928349417c933763cf7a8055e01
lucky <takuto.yoshikai@gmail.com>
2021-01-01 23:07:55 +0900

    views

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function Remocon() {
   app.set("view engine", "ejs");
   let modules = {};
   app.get("/", function(req, res) {
-    res.render(__dirname + "/index.ejs", {
+    res.render(__dirname + "/views/index.ejs", {
       modules: modules
     });
   })
