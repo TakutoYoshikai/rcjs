ccfdd67d686778654f9c64b8968a67253d480e29
lucky <takuto.yoshikai@gmail.com>
2021-01-02 12:34:51 +0900

    var name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const RemoconModuleType = {
 
 function randomString(n) {
   var s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  Array.from(Array(n)).map(()=>S[Math.floor(Math.random()*S.length)]).join('')
+  Array.from(Array(n)).map(()=>s[Math.floor(Math.random()*s.length)]).join('')
 }
 class RemoconModule {
   constructor(id, type) {
